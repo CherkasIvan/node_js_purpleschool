1ae3010980efec2f8ea2a9b986d1a6e0cf15104b
JV13 <cherkas.ivan13@gmail.com>
2025-06-01 16:55:27 +0300

    spawn exec

---
+++ node-11_spawn_exec/app.js
@@ -0,0 +1,26 @@
+const { spawn } = require("child_process");
+
+// const child_process = exec("dir", (err, stdout, stderr) => {
+//   if (err) {
+//     console.error(err.message);
+//   }
+//   console.log(`stdout:  ${stdout}`);
+//   console.log(`stderr:  ${stderr}`);
+// });
+const child_process = spawn("cmd.exe", ["/c", "dir"]);
+// const child_process = spawn(
+//   "dir"
+//   // , (err, stdout, stderr) => {
+//   // if (err) {
+//   //   console.error(err.message);
+//   // }
+//   // console.log(`stdout:  ${stdout}`);
+//   // console.log(`stderr:  ${stderr}`);
+//   // }
+// );
+
+// child_process.on("exit", (code) => console.log(`code ${code}`));
+
+child_process.stdout.on("data", (code) => console.log(`stdout data ${code}`));
+child_process.stderr.on("data", (code) => console.log(`stderr data ${code}`));
+child_process.stdout.on("exit", (code) => console.log(`code exit ${code}`));
