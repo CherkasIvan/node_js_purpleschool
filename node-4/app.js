5ddaacd5ead088445268840f3ba5edfe0ff0eede
JV13 <cherkas.ivan13@gmail.com>
2025-11-24 14:46:20 +0300

    Глобальные переменные

---
+++ node-4/app.js
@@ -0,0 +1,13 @@
+// console.log( 'Текущая дирректория ' + __dirname)
+
+// console.log( 'Текущий файл ' + __filename)
+
+// console.log( global )
+
+// console.log( performance )
+
+// console.log( module )
+
+console.log( exports )
+
+console.log( require )
