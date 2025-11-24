c315523b1bf70e143193b06965f943a5901823bb
JV13 <cherkas.ivan13@gmail.com>
2025-11-24 08:39:27 +0300

    reflog

--- node-12_fork/fork.js
+++
@@ -1,8 +0,0 @@
-process.on("message", (m) => {
-  if (m == "disconnect") {
-    process.disconnect();
-    return;
-  }
-  console.log(`Клиент получил ${m}`);
-  process.send("Pong!");
-});
--- node-3/app.js
+++
@@ -1,9 +0,0 @@
-console.log(__dirname);
-
-console.log(__filename);
-
-// console.log(module);
-
-console.log(exports);
-
-console.log(require);
