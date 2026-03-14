9c50a65cd2a6f2cd8fa01ff03baf1c3664d9b54e
JV13 <cherkas.ivan13@gmail.com>
2026-03-14 03:05:48 +0300

    latest lesson without project

---
+++ node-16/app.js
@@ -0,0 +1,14 @@
+let outter = null
+
+let run = function () {
+  let inner = outter 
+  let unused =function() {
+    if(inner) {
+      console.log('hi') 
+    }
+    outter = {
+      longStr: new Array(10000).join('*')
+    }
+  }
+} 
+setInterval(run, 1000)
